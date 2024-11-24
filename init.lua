957203bb9498ac4a11e421938213837145a0f237
Lukas Linzen <lukas.linzen@protonmail.com>
2024-11-24 20:44:35 +0100

    added tinydignostics

--- init.lua
+++ init.lua
@@ -238,6 +238,8 @@ if not vim.loop.fs_stat(lazypath) then
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
+vim.diagnostic.config { virtual_text = false }
+
 -- [[ Configure and install plugins ]]
 --
 --  To check the current status of your plugins, run
@@ -941,10 +943,10 @@ for _, ft_path in ipairs(vim.api.nvim_get_runtime_file('lua/custom/snippets/*.lu
   loadfile(ft_path)()
 end
 
-vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.tabstop = 2 -- A TAB character looks like 4 spaces
 vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
-vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
-vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+vim.o.softtabstop = 2 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 2 -- Number of spaces inserted when indenting
 
 --- end ::CUSTOM::
 -------------------------------------------------------------------
