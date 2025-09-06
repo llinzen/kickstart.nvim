8ff1dd5ef9a6b1929305a410b3e42e43bd71f9ce
Lukas Linzen <lukas.linzen@protonmail.com>
2025-09-06 20:27:33 +0200

    added formaters

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -27,6 +27,8 @@ return {
       formatters_by_ft = {
         lua = { 'stylua' },
         cpp = { 'clang-format' },
+        bash = { 'beautysh' },
+        markdown = { 'mdformat' },
         -- Conform can also run multiple formatters sequentially
         python = { 'isort', 'black' },
         --
